37e067510f2f2307fd6f36f2ac768fa53dbaf394
Rahul Singh <thesincererahul@gmail.com>
2025-03-31 07:33:51 +0530

    Added signup with zod validations

--- backend/src/schema/user.js
+++ backend/src/schema/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -18,6 +19,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Username is required'],
         unique: [true, 'Username is already exists'],
+        minLength: [3, 'Username must be at least 3 characters long'],
         match: [
             /^[a-zA-Z0-9]+$/,
             'Username must contain only letters and numbers'
@@ -30,6 +32,9 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', function saveUser(next) {
     const user = this;
+    const SALT = bcrypt.genSaltSync(9);
+    const hashedPassword = bcrypt.hashSync(user.password, SALT);
+    user.password = hashedPassword;
     user.avatar = `https://robohash.org/${user.username}`;
     next(); // next middleware
 });
